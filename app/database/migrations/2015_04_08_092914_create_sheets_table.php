2ca0bb987e0f814b3472a2ea78efd8ee9c8c73c7
Ruchir <ruchir.kakkad@gmail.com>
2015-04-08 15:51:26 +0530

    test branch

--- app/database/migrations/2015_04_08_092914_create_sheets_table.php
+++ app/database/migrations/2015_04_08_092914_create_sheets_table.php
@@ -17,8 +17,19 @@ class CreateSheetsTable extends Migration {
 			$table->increments('id');
             $table->date('input_date');
             $table->integer('marketing_countries_id');
+            $table->foreign('marketing_countries_id')
+                ->references('marketing_countries_id')->on('marketing_countries')
+                ->onDelete('cascade');
             $table->integer('marketing_categories_id');
+            $table->foreign('marketing_categories_id')
+                ->references('marketing_categories_id')->on('marketing_categories')
+                ->onDelete('cascade');
             $table->integer('users_id');
+            $table->foreign('users_id')
+                    ->references('users_id')->on('users')
+                    ->onDelete('cascade');
+
+
 			$table->timestamps();
             $table->softDeletes();
 		});
