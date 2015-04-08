4fb6a30508f09fedc154ca18dff82a6205948de2
Ruchir <ruchir.kakkad@gmail.com>
2015-04-08 16:52:35 +0530

    sheets table schema created

--- app/database/migrations/2015_04_08_092914_create_sheets_table.php
+++ app/database/migrations/2015_04_08_092914_create_sheets_table.php
@@ -16,17 +16,23 @@ class CreateSheetsTable extends Migration {
 		{
 			$table->increments('id');
             $table->date('input_date');
-            $table->integer('marketing_countries_id');
+
+
+            $table->unsignedInteger('marketing_countries_id');
             $table->foreign('marketing_countries_id')
                 ->references('marketing_countries_id')->on('marketing_countries')
                 ->onDelete('cascade');
-            $table->integer('marketing_categories_id');
+
+
+            $table->unsignedInteger('marketing_categories_id');
             $table->foreign('marketing_categories_id')
                 ->references('marketing_categories_id')->on('marketing_categories')
                 ->onDelete('cascade');
-            $table->integer('users_id');
-            $table->foreign('users_id')
-                    ->references('users_id')->on('users')
+
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                    ->references('user_id')->on('users')
                     ->onDelete('cascade');
 
 
